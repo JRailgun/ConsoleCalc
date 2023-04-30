f8502062c693d292ff27935e82a10e52ee3462c2
Yaroslav <yaroslav.ananichev@mail.ru>
2023-04-30 16:37:35 +0300

    Первый коммит

---
+++ ISIS5/Program.cs
@@ -0,0 +1,28 @@
+﻿double a = 0; double b = 0;
+
+Console.WriteLine("Введите 1-ое число");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число");
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Выберите действие:");
+Console.WriteLine("\t+ - Сложение");           
+Console.WriteLine("\t- - Вычитание");           
+Console.WriteLine("\t* - Умножение");          
+Console.WriteLine("\t/ - Деление");        
+Console.Write("Ваше действие: ");
+
+switch (Console.ReadLine())           
+{                
+    case "+":                   
+        Console.WriteLine($"Результат: {a} + {b} = " + (a + b));                    
+        break;               
+    case "-":                    
+        Console.WriteLine($"Результат: {a} - {b} = " + (a - b));                   
+        break;                
+    case "*":                    
+        Console.WriteLine($"Результат: {a} * {b} = " + (a * b));                    
+        break;                
+    case "/":                    
+        Console.WriteLine($"Результат: {a} / {b} = " + (a / b));                  
+        break;            
+}
